e62887cce32357e89638462cc6fecafe5bb43a33
Parthiban D <parthiband2020@gmail.com>
2025-09-05 11:54:20 +0530

    Deployment AWS

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -6,7 +6,6 @@ from sklearn.ensemble import (AdaBoostRegressor,GradientBoostingRegressor,Random
 from catboost import CatBoostRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.neighbors import KNeighborsRegressor
 from xgboost import XGBRegressor
 
 from sklearn.metrics import r2_score
@@ -33,13 +32,50 @@ class Model_Trainer:
                 "Gradient Boost":GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
                 "XGB": XGBRegressor(),
-                "KNN":KNeighborsRegressor(),
                 "Cat Boost": CatBoostRegressor(),
                 "Ada Boost": AdaBoostRegressor()
             }
 
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128]
+                },
+                "Gradient Boost":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128]
+                },
+                "Linear Regression":{},
+                "XGB":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128]
+                },
+                "Cat Boost":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "Ada Boost":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128]
+                }
+                
+            }
+
             model_report:dict=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                             models=models)
+                                            models=models,params=params)
             # To get the best model score from dict
             best_model_score= max(sorted(model_report.values()))
             # To get the best model name from dict
--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import RandomizedSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -16,12 +17,20 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException (e,sys)
     
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,params):
     try:
         report={}
         for i in range(len(list(models))):
             model= list(models.values())[i]
+            para=params[list(models.keys())[i]]
+
+            rs = RandomizedSearchCV(model,para,cv=3)
+            rs.fit(X_train,y_train)
+
+            model.set_params(**rs.best_params_)
             model.fit(X_train,y_train)
+
+            #model.fit(X_train,y_train)
 
             y_pred_train=model.predict(X_train)
             y_pred_test=model.predict(X_test)
@@ -31,7 +40,15 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
 
             report[list(models.keys())[i]]=test_model_score
 
-            return report
+        return report
     
     except Exception as e:
         raise CustomException (e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as f:
+            return dill.load(f)
+    except Exception as e:
+        raise CustomException (e,sys)
+
